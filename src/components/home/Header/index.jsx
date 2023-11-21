1e54056889075b99263fc01bc815298e042e210e
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-21 01:21:47 +0100

    setting up the home page header.

---
+++ src/components/home/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+ return (
+  <header className='home_header'>
+   <h2>Inc. This Morning</h2>
+   <h1>
+    <span>
+     "<span>Blog</span>"
+    </span>
+   </h1>
+   <p>
+    Awesome place to make oneself <br /> productive and entertained through
+    daily updates.
+   </p>
+  </header>
+ );
+};
+
+export default Header;
