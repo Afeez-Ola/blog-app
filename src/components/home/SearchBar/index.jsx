2dedb2ebcba17a4f18da7a1a2761c026df577e34
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-21 01:34:51 +0100

    .

--- src/components/home/SearchBar/index.jsx
+++ src/components/home/SearchBar/index.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 const SearchBar = (props) => {
  return (
   <div className='searchBar_wrapper'>
-   <form>
-    <input type='text' onChange={props.handleSearch} placeholder='Search by Category' value={props.value} />
+   <form onSubmit={props.formSubmit}>
+    <input
+     type='text'
+     onChange={props.handleSearch}
+     placeholder='Search by Category'
+     value={props.value}
+    />
+    {props.value && <span onClick={props.clearSearch}>X</span>}
+    <button>Go</button>
    </form>
   </div>
  );
