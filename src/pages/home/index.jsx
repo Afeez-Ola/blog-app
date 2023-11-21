5c6322669ceab8977441a4c555a40682f27e928d
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-21 01:28:03 +0100

    setting up search bar component.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../../components/home/Header';
 
 const Home = () => {
  return (
   <div>
-   <h1>Home Page</h1>
+   <Header></Header>
   </div>
  );
 };
